dffe3db66f2228858c4fb7aefb339b042385ae8e
demkantor <demkantor@gmail.com>
2020-06-17 15:17:15 -0500

    api fetch call working and brought to components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React, { Component } from 'react';
 
 import { Cards, Chart, Country } from './components';
-import styles from './App.module.css'
+import styles from './App.module.css';
+
+import { fetchData } from './api';
+
 
 class App extends Component {
+
+  state = {
+    data: {}
+  };
+
+  componentDidMount = async () => {
+    const data = await fetchData();
+
+    this.setState({ data });
+  }
   render() {
+    const { data } = this.state;
+
     return (
       <div className={styles.container}>
-        <Cards />
+        <Cards data={data} />
         <Country />
         <Chart />
       </div>
