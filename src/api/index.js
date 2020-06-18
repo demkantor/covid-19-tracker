62008e375ead6eac43600edd8271afae23618cb4
demkantor <demkantor@gmail.com>
2020-06-18 00:00:13 -0500

    added display cards, started charts

--- src/api/index.js
+++ src/api/index.js
@@ -5,11 +5,22 @@ const url = 'https://covid19.mathdro.id/api';
 export const fetchData = async () => {
     try {
         const { data: { confirmed, recovered, deaths, lastUpdate } } = await axios.get(url);
+        // console.log(confirmed, recovered, deaths, lastUpdate);
 
         return { confirmed, recovered, deaths, lastUpdate };
 
     } catch (error) {
-        console.error(error);
+        console.error('error fetching main data set', error);
 
     };
 };
+
+export const fetchDailyData = async () => {
+    try {
+        const { data } = await axios.get(`${url}/daily`);
+        // console.log(data);
+        
+    } catch (error) {
+        console.error('error fetching daily data set', error);
+    }
+}
--- src/components/Chart/Chart.jsx
+++ src/components/Chart/Chart.jsx
@@ -1,6 +1,25 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { fetchDailyData } from '../../api';
+import { Line, Bar } from 'react-chartjs-2';
+
+import styles from './Chart.module.css';
 
 const Chart = () => {
+
+    const [dailyData, setDailyData] = useState({});
+
+    useEffect(() => {
+        const fetchApi = async () => {
+            setDailyData(await fetchDailyData());
+        };
+
+        fetchApi();
+    });
+
+    const lineChart = (
+
+    );
+
     return (
         <div>
             <h1>Chart</h1>
